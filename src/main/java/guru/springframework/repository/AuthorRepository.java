1ea2a92b41635f6f80c7053cb92fe85b46bbff56
craigb <BrandonCraig@rentalcars.com>
2020-04-07 13:20:54 +0100

    Up to Repositories

---
+++ src/main/java/guru/springframework/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.repository;
+
+import guru.springframework.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
