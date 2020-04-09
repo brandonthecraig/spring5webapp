65e8dc5fce2711ce543cb626d892a421d9b24f66
craigb <BrandonCraig@rentalcars.com>
2020-04-09 16:40:44 +0100

    Resyncing my weird ass bs to the main setup.
    - watch your naming and directories ya dummy ;)

--- src/main/java/guru/springframework/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.repositories;
-
-import guru.springframework.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-}
--- src/main/java/guru/springframework/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.repositories;
-
-import guru.springframework.domain.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository <Book, Long> {
-}
